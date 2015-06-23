b5d3426b03dbb5c2e1db653d02f7237b24fdabb7
Andy Reading <andy@castlegateit.co.uk>
2015-06-23 13:13:38 +0100

    Initial commit.

---
+++ cgit-wp-responsive-video.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+
+Plugin Name: Castlegate IT WP Responsive Video
+Plugin URI: http://github.com/castlegateit/cgit-wp-responsive-video
+Description: Embeds videos responsively when embedding in post content.
+Version: 1.1
+Author: Castlegate IT
+Author URI: http://www.castlegateit.co.uk/
+License: MIT
+
+*/
+
+
+/**
+ * Filters the embed code HTML provided by WordPress to something more
+ * appropriate for responsive websites. Detects the embed type and called the
+ * specific embed function.
+ *
+ * @param string $html HTML embed code
+ * @param string $url  Media URL
+ * @param array  $args Array of embed arguments
+ *
+ * @author Andy Reading <andy@castlegateit.co.uk>
+ *
+ * @return string HTML embed code
+ */
+function cgit_wp_responsive_video_embed($html, $url, $args) {
+
+    // Supported embedded services
+    $supported = array(
+        'youtube',
+        'vimeo'
+    );
+
+    // Calculate embed ratio
+    $width = $args['width'];
+    $height = $args['height'];
+    $ratio = $width / $height;
+
+    // Check the embed matches a supported service and return the embed code
+    foreach ($supported as $service) {
+
+        $detect = 'cgit_wp_responsive_video_detect_' . $service;
+        $embed = 'cgit_wp_responsive_video_embed_' . $service;
+
+        if ($code = $detect($url)) {
+            return $embed($code, $ratio);
+        }
+    }
+
+    // No support, return unmodified code
+    return $html;
+}
+
+add_filter('embed_oembed_html','cgit_wp_responsive_video_embed', 10, 3);
+
+
+/**
+ * Enqueue the custom styles
+ */
+function cgit_wp_responsive_video_enqueue() {
+    wp_enqueue_style(
+        'cgit-wp-responsive-video',
+        plugins_url('css/video-styles.css', __FILE__ ),
+        '1'
+    );
+}
+add_action('wp_enqueue_scripts', 'cgit_wp_responsive_video_enqueue');
+
+
+/**
+ * Takes a media URL and attempts to determine if it is a YouTube video,
+ * returning the YouTube ID
+ *
+ * @param string $url Media URL
+ *
+ * @author Andy Reading <andy@castlegateit.co.uk>
+ *
+ * @return string YouTube ID
+ */
+function cgit_wp_responsive_video_detect_youtube($url) {
+
+    $regex = '%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|';
+    $regex.= '(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i';
+
+    if (preg_match($regex, $url, $match)) {
+        return $match[1];
+    }
+
+    return false;
+}
+
+
+/**
+ * Returns YouTube embed code, optimised for responsive code.
+ *
+ * @param string  $code  YouTube ID
+ * @param integer $ratio Width/height ratio
+ *
+ * @author Andy Reading <andy@castlegateit.co.uk>
+ *
+ * @return string HTML embed code
+ */
+function cgit_wp_responsive_video_embed_youtube($code, $ratio) {
+
+    $padding = ' style="padding-bottom:' . round($ratio * 100, 2) . '%"';
+
+    $return = '<div class="cgit-wp-responsive-video"' . $padding . '>' . "\n";
+    $return.= '    <iframe src="//www.youtube.com/embed/';
+    $return.= $code . '" frameborder="0" allowfullscreen></iframe>' . "\n";
+    $return.= '</div>';
+
+    return $return;
+}
+
+
+/**
+ * Takes a media URL and attempts to determine if it is a Vimeo video,
+ * returning the Vimeo ID
+ *
+ * @param string $url Media URL
+ *
+ * @author Andy Reading <andy@castlegateit.co.uk>
+ *
+ * @return string Vimeo ID
+ */
+function cgit_wp_responsive_video_detect_vimeo($url) {
+
+    $regex = '/^(http:\/\/)?(www\.)?vimeo\.com\/([\d]{1,9})$/i';
+    $regex_embed = '/player\.vimeo\.com\/video\/([\d]{1,9})"/i';
+
+    if (preg_match($regex, $url, $match)) {
+        // Normal link
+        return $match[3];
+    }
+    else {
+        // Embed link
+        if (preg_match($regex_embed, $url, $match)) {
+            return $match[1];
+        }
+    }
+
+    return false;
+}
+
+
+/**
+ * Returns YouTube embed code, optimised for responsive sites.
+ *
+ * @param string  $code  Vimeo ID
+ * @param integer $ratio Width/height ratio
+ *
+ * @author Andy Reading <andy@castlegateit.co.uk>
+ *
+ * @return string HTML embed code
+ */
+function cgit_wp_responsive_video_embed_vimeo($code, $ratio) {
+
+    $padding = ' style="padding-bottom:' . round($ratio * 100, 2) . '%"';
+
+    $params = '?portrait=0&amp;byline=0&amp;badge=0&amp;color=E70871';
+
+    $return = '<div class="cgit-wp-responsive-video"' . $padding . '>' . "\n";
+    $return.= '   <iframe src="//player.vimeo.com/video/' . $code . $params;
+    $return.= '" frameborder="0" webkitallowfullscreen mozallowfullscreen ';
+    $return.= 'allowfullscreen></iframe>' . "\n" . '</div>';
+
+    return $return;
+}
