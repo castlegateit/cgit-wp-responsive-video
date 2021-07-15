e415e1a6781c1e02d2aa8afefcbb2b7d77bdb2e7
Lamé Moatshe <lame@castlegateit.co.uk>
2021-07-15 11:23:03 +0100

    updating plugin version number

--- cgit-wp-responsive-video.php
+++ cgit-wp-responsive-video.php
@@ -5,7 +5,7 @@
 Plugin Name: Castlegate IT WP Responsive Video
 Plugin URI: http://github.com/castlegateit/cgit-wp-responsive-video
 Description: Embeds videos responsively when embedding in post content.
-Version: 1.1
+Version: 1.2
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
